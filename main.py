79a503977a248905641558505ea91fe92f255e9b
eclipse7723 <eclipse7723@gmail.com>
2023-01-03 14:45:31 +0100

    add prepare_managers

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from src.App import App
 from src.ui.interface_manager import InterfaceManager
 
 
+def prepare_managers():
+    from src.devices.device_manager import DeviceManager
+    DeviceManager.create_devices()
+
+
 if __name__ == "__main__":
-    interface = InterfaceManager.get_interface("gui")
+    prepare_managers()
+
+    interface = InterfaceManager.get_interface("cli")
     app = App(interface)
 
     app.run()
