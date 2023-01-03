ed539afc8cceab0ff8d9d36435efba78202d37b9
eclipse7723 <eclipse7723@gmail.com>
2023-01-03 13:46:33 +0100

    pass the nate to created widget

--- src/ui/gui/components/frame.py
+++ src/ui/gui/components/frame.py
@@ -4,11 +4,11 @@ import tkinter as tk
 class Frame(tk.Frame):
     def __init__(self, *args, **kwargs):
         super(Frame, self).__init__(*args, **kwargs)
-        self.name = kwargs["name"]
+        self.name = kwargs.get("name", "undefined")
         self.__widgets = {}
 
     def create_widget(self, name, type_widget, **params):
-        widget = type_widget(master=self, **params)
+        widget = type_widget(master=self, name=name, **params)
         widget.pack(pack=params.get("pack_side", tk.LEFT), fill=params.get("pack_fill", tk.NONE))
         self.add_widget(name, widget)
         return widget
