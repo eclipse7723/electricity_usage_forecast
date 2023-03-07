1706ced7a6415f8f615ee8cc5fc90d0fd743259d
eclipse7723 <eclipse7723@gmail.com>
2023-03-07 01:28:47 +0100

    add 'show commands' command

--- src/ui/cli/components/command_tree.py
+++ src/ui/cli/components/command_tree.py
@@ -11,6 +11,7 @@ class CommandTree:
         self.initialize()
 
     def initialize(self):
+        self.add_command("show commands", "shows all available commands", self.show_commands)
         for command in CommandManager.all():
             command.set_interface(self._interface)
             self.commands[command.alias] = command
@@ -47,6 +48,22 @@ class CommandTree:
 
         command = self.commands[name]
         return command
+
+    def add_command(self, alias, description, func):
+        command_class_name = "Command"+"".join([w.title() for w in alias.split(" ")])
+
+        command = CommandManager.create(
+            name=command_class_name,
+            alias=alias,
+            description=description,
+            func=func
+        )
+
+        if command is None:
+            return False
+
+        self.commands[command.alias] = command
+        return True
 
     def show_commands(self):
         print("All available commands:")
