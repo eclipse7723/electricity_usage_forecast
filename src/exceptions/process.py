2b936e2a0bf1bb26e2ea96ecc92fea0a10c9f331
eclipse7723 <eclipse7723@gmail.com>
2023-03-09 01:27:10 +0100

    add base exception

--- src/exceptions/process.py
+++ src/exceptions/process.py
@@ -1,10 +1,14 @@
-class AlreadyRunningError(Exception):
+class ProcessException(Exception):
+    pass
+
+
+class AlreadyRunningError(ProcessException):
     def __init__(self, process_name):
         exception_message = f"Process {process_name!r} is already run"
         super(AlreadyRunningError, self).__init__(exception_message)
 
 
-class AlreadyStoppedError(Exception):
+class AlreadyStoppedError(ProcessException):
     def __init__(self, process_name):
         exception_message = f"Process {process_name!r} is already stop"
         super(AlreadyStoppedError, self).__init__(exception_message)
