9e53782876d8428175c62bd29b0ee0ee05b1a50c
eclipse7723 <eclipse7723@gmail.com>
2023-03-11 01:50:31 +0100

    wip + fix asserts

--- src/tests/device_controller.py
+++ src/tests/device_controller.py
@@ -1,6 +1,7 @@
 import unittest
 from src.devices.device_controller import DeviceController
-from src.tests.data.test_data import RandomDeviceGenerator
+from src.tests.data.test_data import RandomDeviceGenerator, DAYS_PERIOD
+from src.model.period import Period
 
 
 TEST_DEVICES_COUNT = 3
@@ -12,6 +13,8 @@ class DeviceControllerTests(unittest.TestCase):
     def setUp(self):
         self.controller = DeviceController()
         self._randomizer = RandomDeviceGenerator(self.controller)
+
+        Period.update_days(DAYS_PERIOD)
 
         for i in range(1, TEST_DEVICES_COUNT + 1):
             params, device = self._randomizer(allow_problem_params=False)
@@ -33,12 +36,12 @@ class DeviceControllerTests(unittest.TestCase):
 
         # check if save has been created
         from os import path
-        self.assertFalse(path.exists(TEST_SAVE_PATH), "Save devices not found")
+        self.assertTrue(path.exists(TEST_SAVE_PATH), "Save devices not found")
 
         # disconnect all devices and check size
         self.controller.remove_all()
         _connected_devices = self.controller.get_devices()
-        self.assertNotEqual(0, len(_connected_devices), "Wrong number of connected devices")
+        self.assertEqual(0, len(_connected_devices), "Wrong number of connected devices")
 
         # load and connect previous devices
         self.controller.load_devices()
@@ -55,5 +58,14 @@ class DeviceControllerTests(unittest.TestCase):
     def _check_loaded_device(self, device):
         params_number = self._randomizer.extract_random_device_number(device.identity)
         initial_params = self._randomizer.get(params_number)
+        actual_params = device.get_save()[device.identity]
 
-        # todo: check params and assert
+        for key, value in initial_params.items():
+            if key == "kWh":
+                # convert to watt
+                key = "watt"
+                value *= 1000.0
+
+            actual_value = actual_params[key]
+            self.assertEqual(value, actual_value, f"{device.identity}: wrong load {key!r}")
+
